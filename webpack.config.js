2244f7c7362d9855401ae7027dc3a3bd5d5cf8e4
Joon-S <niceweather94@gmail.com>
2020-05-31 20:44:31 +0900

    Completed 3-plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const webpack = require('webpack')
 
 module.exports = {
   mode: "development",
@@ -24,7 +28,26 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+      new webpack.BannerPlugin({
+        banner: `
+          ${new Date().toLocaleString()}
+        `
+      }),
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        templateParameters: {
+          env: process.env.NODE_ENV === 'development' ? '(개발용)' : ''
+        },
+        minify: process.env.NODE_ENV === 'production' ? {
+          collapseWhitespace: true,
+          removeComments: true
+        } : false
+      }),
+      new CleanWebpackPlugin(),
+      ...(process.env.NODE_ENV === 'production' ? [new MiniCssExtractPlugin({filename: '[name].css'})] : [])
+  ]
   /**
    * TODO: 아래 플러그인을 추가해서 번들 결과를 만들어 보세요.
    * 1. BannerPlugin: 결과물에 빌드 시간을 출력하세요.
